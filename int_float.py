86862884751841b3e5a5494d6b17500c3a3c37bb
KunalGautam214 <kunalgautam214@gmail.com>
2025-01-14 11:36:47 +0530

    Working with integer and float

---
+++ int_float.py
@@ -0,0 +1,55 @@
+
+num = 2
+print(type(num))
+
+num = 2.14
+print(type(num))
+
+# Arithmetic Operation
+#
+# 3 + 2
+# 3 - 2
+# 3 * 2
+# 3 / 2
+# 3 // 2
+# 3 ** 2
+# 3 % 2
+
+print(3 * (2 + 1))
+
+num = 2
+num = num + 1
+print(num)
+
+num = 2
+num *= 10
+print(num)
+
+num = -2
+print(abs(num))
+
+num = 3.75
+print(round(num))
+
+num = 3.75
+print(round(num, 1))
+
+
+# Comparison Operation
+#
+# 3 == 2
+# 3 != 2
+# 3 > 2
+# 3 < 2
+# 3 >= 2
+# 3 <= 2
+
+num_1 = 3
+num_2 = 2
+print(num_1 <= num_2)
+
+num_1 = '100'
+num_2 = '200'
+num_1 = int(num_1)
+num_2 = int(num_2)
+print(num_1 + num_2)
